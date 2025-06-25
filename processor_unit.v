4983760ecc02053488b608e4327edb59cf23c503
Ayesha akhtar <160256531+messy-masterpeice@users.noreply.github.com>
2025-06-25 15:03:51 +0500

    Add files via upload

---
+++ processor_unit.v
@@ -0,0 +1,68 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:28:03 06/25/2025 
+// Design Name: 
+// Module Name:    processor_unit 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module processor_unit (
+    input [7:0] data_in,
+    input trigger, reset, clk,
+    output reg finished,
+    output reg [7:0] result
+);
+
+    integer idx;
+    reg write_en;
+    parameter mem_size = 16;
+
+    // ROM
+    reg [7:0] ROM [0:mem_size-1];
+    initial begin
+        $readmemh("hexFile.mem", ROM);
+    end
+
+    // RAM
+    reg [7:0] RAM [0:mem_size-1];
+
+    always @(posedge clk) begin
+        if (reset) begin
+            finished <= 0;
+            idx <= 0;
+            write_en <= 1;
+        end else begin
+            if (trigger) begin
+                if (idx < mem_size) begin
+                    if (write_en) begin
+                        RAM[idx] <= data_in;
+                        idx <= idx + 1;
+                    end else begin
+                        result <= RAM[idx] * ROM[idx];
+                        idx <= idx + 1;
+                        if (idx == mem_size - 1)
+                            finished <= 1;
+                    end
+                end else begin
+                    if (!finished) begin
+                        write_en <= 0;
+                        idx <= 0;
+                    end
+                end
+            end
+        end
+    end
+
+endmodule
---
+++ processor_unit_tb.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   14:29:06 06/25/2025
+// Design Name:   processor_unit
+// Module Name:   C:/Users/Lenovo/Documents/Downloads/ayeshatask/processor_unit_tb.v
+// Project Name:  ayeshatask
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: processor_unit
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module processor_unit_tb;
+
+    // Inputs
+    reg [7:0] data_in;
+    reg trigger;
+    reg reset;
+    reg clk;
+
+    // Outputs
+    wire finished;
+    wire [7:0] result;
+
+    // Instantiate the Unit Under Test
+    processor_unit uut (
+        .data_in(data_in),
+        .trigger(trigger),
+        .reset(reset),
+        .clk(clk),
+        .finished(finished),
+        .result(result)
+    );
+
+    // Clock Generation
+    always #10 clk = ~clk;
+
+    initial begin
+        data_in = 0;
+        trigger = 0;
+        reset = 1;
+        clk = 0;
+
+        #100;
+        reset = 0;
+        trigger = 1;
+
+        data_in = 8'd9;    // RAM[0]
+        data_in = 8'd21;   // RAM[1]
+        data_in = 8'd105;  // RAM[2]
+        data_in = 8'd134;  // RAM[3]
+        data_in = 8'd7;    // RAM[4]
+        data_in = 8'd3;    // RAM[5]
+        data_in = 8'd19;   // RAM[6]
+        data_in = 8'd29;   // RAM[7]
+        data_in = 8'd14;   // RAM[8]
+        data_in = 8'd27;   // RAM[9]
+        data_in = 8'd8;    // RAM[10]
+        data_in = 8'd20;   // RAM[11]
+        data_in = 8'd24;   // RAM[12]
+        data_in = 8'd30;   // RAM[13]
+        data_in = 8'd40;   // RAM[14]
+        data_in = 8'd36;   // RAM[15]
+
+
+        // Simulation pause
+        #200;
+    end
+
+endmodule
+
